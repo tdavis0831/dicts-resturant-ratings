296c005a5cc06e30f7a19d1c97bd919c45fde3f0
Tasha Davis <tdavis0831@gmail.com>
2021-07-30 02:23:25 +0000

    intital commit

--- ratings.py
+++ ratings.py
@@ -1,4 +1,12 @@
 """Restaurant rating lister."""
 
 
-# put your code here
+#create diction from scores
+#open file
+#index 
+
+#for name, rating in dictname.items():
+    #sorted(dictname)
+    #print(f" {name} is rated a {rating})
+#f string to say name of place is rate a rating
+
